99fab86cdf85f493c34e64de4101a9c37ba34928
clauddian <claudia.anissa.95@gmail.com>
2023-11-21 12:36:23 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          title: Text("Layout Flutter"),
+        ),
+        body: SafeArea(
+          child: Row(children: <Widget>[
+            Column(children: <Widget>[
+              buatKotak(Color.fromARGB(255, 6, 54, 31), 100),
+              buatKotak(Colors.orangeAccent[400]!, 70),
+              buatKotak(Colors.greenAccent, 50),
+              buatKotak(Colors.orangeAccent[400]!, 90),
+            ])
+          ]),
+        )),
+  ));
+}
+
+Widget buatKotak(Color warna, double ukuran) {
+  return Container(
+    decoration: BoxDecoration(color: warna),
+    height: ukuran,
+    width: ukuran,
+    margin: const EdgeInsets.all(10),
+  );
+}
